e5c736f2ca47f2a0a8e276ceaba897da1c712190
gitclone-url <gamhakim2@gmail.com>
2023-09-28 16:28:22 +0600

    Sky: Bringup derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sky.mk
+    $(LOCAL_DIR)/derp_sky.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sky-eng \
-    lineage_sky-userdebug \
-    lineage_sky-user
+    derp_sky-eng \
+    derp_sky-userdebug \
+    derp_sky-user
--- derp_sky.mk
+++ derp_sky.mk
@@ -8,20 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common derp stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1440
+DERP_BUILDTYPE := Unofficial
 
 # Inherit from sky device.
 $(call inherit-product, device/xiaomi/sky/device.mk)
 
-## Inherit ih8sn Makefile
+# Inherit ih8sn Makefile
 $(call inherit-product, vendor/extra/ih8sn/product.mk)
 
-## Device identifier
+# Device identifier
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := sky
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_sky
+PRODUCT_NAME := derp_sky
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
